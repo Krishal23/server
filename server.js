456f4ce75fc4de2551533f52356076f24c5850f4
Krishal23 <kanhakesharwani23@gmail.com>
2024-11-21 01:24:46 +0530

    ads

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ const verifyMembership = async (req, res, next) => {
 
 // Signup Route
 app.post('/signup', async (req, res) => {
+    console.log("hello")
     const { username, email, password } = req.body;
     console.log("Username:", username);
     console.log("Email:", email);
